30b1bf72badc01cd240f4c6e1f4eff3514b94ad7
shubham <shubhamkatariya05@gmail.com>
2019-11-26 19:14:02 -0800

    build changes

---
+++ client/build/precache-manifest.23451a4c180353bf446fbad8fad646a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca47ad129f8a9f4e88a496a8effad53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c36968bd251de9f58ba",
+    "url": "/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "992ad656cdd58fa74106",
+    "url": "/static/css/main.44734201.chunk.css"
+  },
+  {
+    "revision": "9c36968bd251de9f58ba",
+    "url": "/static/js/2.245ddac9.chunk.js"
+  },
+  {
+    "revision": "992ad656cdd58fa74106",
+    "url": "/static/js/main.e17b2034.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
